bbf883203fb42a690173624038d5e25dbac58fb6
Xyruz Reyn Aninon <sibatmanaymaitim@gmail.com>
2026-06-14 08:51:59 +0800

    Initial commit

---
+++ api/ios/852.php
@@ -0,0 +1,17 @@
+<?php
+
+header("Content-Type: text/plain");
+
+echo <<<TEXT
+[General]
+bypass-system = true
+
+[Rule]
+FINAL,DIRECT
+
+[Host]
+growtopia1.com = 5.39.46.39
+growtopia2.com = 5.39.46.39
+www.growtopia1.com = 5.39.46.39
+www.growtopia2.com = 5.39.46.39
+TEXT;
