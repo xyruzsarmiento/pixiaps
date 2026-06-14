8c4e6f7e771b023f4cb49d569434bc50f08aa689
Xyruz Reyn Aninon <sibatmanaymaitim@gmail.com>
2026-06-14 09:02:37 +0800

    Update and rename 852.php to 852.js

---
+++ api/android/852.js
@@ -0,0 +1,11 @@
+export default function handler(req, res) {
+  res.status(200);
+  res.setHeader("Content-Type", "text/plain; charset=utf-8");
+  res.setHeader("Content-Disposition", "inline");
+
+  res.end(`
+growtopia1.com = 5.39.46.39
+growtopia2.com = 5.39.46.39
+www.growtopia1.com = 5.39.46.39
+www.growtopia2.com = 5.39.46.39`);
+}
